df45f94545d0160dcf45e93d0d9af03969e3388f
Pavithrakuruba <103957063+Pavithrakuruba@users.noreply.github.com>
2022-06-30 11:30:15 +0530

    Add files via upload

---
+++ dictQassending.py
@@ -0,0 +1,31 @@
+# a={1:2,3:4,4:3,2:1,0:0}
+# b=[]
+# for i in a:
+#     b.append([i,a[i]])
+# print(b)
+# for i in range(0,(len(b)-1)):
+#     for j in range(0,len(b)-1):
+#         if b[j][1]>b[j+1][1]:
+#             b[j][1],b[j+1][1]=b[j+1][1],b[j][1]
+# print(b)
+
+
+# a={"a":51,"b":40,"c":38,"d":11}
+# a={1:45,2:31,3:12,4:67}
+# b=[]
+# for i in a:
+#     b.append([i,a[i]])
+# print(len(b))
+# for i in range(0,len(b)-1):
+#     for j in range(0,len(b)-1):
+#         if b[j][1]>b[j+1][1]:
+#             b[j][1],b[j+1][1]=b[j+1][1],b[j][1]
+# print(b)
+
+
+# a={"pavi","riya"}
+# b={}
+# for i in a:
+#     b.update({i:len(i)})
+# print(b)
+
---
+++ dictQdisending.py
@@ -0,0 +1,14 @@
+# a={1:2,3:4,4:3,2:1,0:0,5:9}
+# b=[]
+# for i in a:
+#     b.append([i,a[i]])                                                                                                                                                                          
+# # print(b)
+# for i in range(0,len(b)-1):
+#     for j in range(0,len(b)-1):
+#         if b[j][1]<b[j+1][1]:
+#             b[j][1],b[j+1][1]=b[j+1][1],b[j][1]
+# print(b)
+
+
+
+
